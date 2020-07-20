75d4028645df87cee0bc555874d0f70d6688c021
domssilva <silva@programmer.net>
2020-07-20 14:21:53 +0200

    init C# cookbook

---
+++ csharp/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            string name = "C#";
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
