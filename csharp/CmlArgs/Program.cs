f47e300993854ab449a5299533c1ef72d1071bdb
domssilva <silva@programmer.net>
2020-07-21 11:28:45 +0200

    [C#] command line arguments

---
+++ csharp/CmlArgs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CmlArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter an argument");
+            }
+            else 
+            {
+                int idx = 0;
+                foreach(string argument in args)
+                {
+                    Console.WriteLine($"argument[{idx}] = {argument}");
+                    idx++;
+                }
+            }
+        }
+    }
+}
