717ddf41f7289afd5fe1904a65a9c97f9a4d5048
domssilva <silva@programmer.net>
2020-10-04 23:23:03 +0200

    [go] - handle routes with mux (http request multiplexer)

--- go/server/main.go
+++ go/server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -10,13 +12,30 @@ type HttpHandler struct{}
 // implement ServeHTTP method on my handler
 func (h HttpHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	// binary data for response
-	data := []byte("Welcome to my server.")
+	data := []byte("Welcome to my server.\n")
 
 	// send data as response
 	res.Write(data)
+
+	io.WriteString(res, "What can I offer you?")
 }
 
 func main() {
+	mux := http.NewServeMux()
+
+	//handle routes
+	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(res, "home page")
+	})
+
+	mux.HandleFunc("/help", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(res, "help page")
+	})
+
+	http.ListenAndServe(":5003", mux)
+}
+
+func allRequests() {
 	requestHandler := HttpHandler{}
 
 	// start HTTP server + lock the process
