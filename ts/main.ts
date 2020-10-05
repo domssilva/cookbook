a460c18d89cae352fcf5efd89816fd96739e7c7b
domssilva <silva@programmer.net>
2020-10-05 18:09:07 +0200

    [ts] - vanilla node server

--- ts/main.ts
+++ ts/main.ts
@@ -1,39 +1,10 @@
-/*
-  Tutorial: https://kushagra.dev/blog/build-git-learn-git
+import * as http from 'http';
 
-  Git is best thought of as a tool for storing the history 
-  of a collection of files.
-  And think of repositories as a collection of everything related to Git.
-*/
-let repo;
+const PORT = 4003;
+const server = http.createServer((req, res) => {
+  console.log('Request received.');
+});
 
-function Git(name) {
-  // returns repository
-  this.name = name;
-
-  // id counter: keeps track of last commit
-  this.lastCommitId = -1;
-}
-
-repo = new Git('my-repository');
-
-/*
-  COMMITS are a snapshot of your project's contents.
-  It is these commits which when chained together form the history of your project.
-*/
-
-function Commit(id, message) {
-  this.id = id;
-  this.message = message;
-}
-
-Git.prototype.commit = function(message) {
-  //In actual Git, commit id is a 40-hexdigit number also called as "SHA-1 id". But for keeping things simple we are using integers here.
-  let commit = new Commit(++this.lastCommitId, message);
-  return commit;
-}
-
-// git commit -m 'init repository'
-repo.commit('init repository');
-
-console.log(repo);
+server.listen(PORT, () => {
+  console.log('Serfver listening on port: ', PORT);
+});
