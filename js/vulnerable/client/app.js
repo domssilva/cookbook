d1b95b4d12dc28f697ff40afcd520de23489f8e4
domssilva <silva@programmer.net>
2020-10-21 23:55:53 +0200

    [js] xss route

--- js/vulnerable/client/app.js
+++ js/vulnerable/client/app.js
@@ -3,9 +3,14 @@ const app = express();
 
 const PORT = 3001;
 
-//app.get('/', () => {});
+//app.get('/', (req, res) => {});
 app.get('/', (req, res) => {
   res.send('hello');
+});
+
+app.get('/xss', (req, res) => {
+  let name = req.query.name; 
+  res.send(`hello ${name}`);
 });
 
 app.listen(PORT, () => console.log(`LISTENING ON PORT ${PORT}`));
