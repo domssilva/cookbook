dfc69d3d33bba213c442889a773d70c4e245fea4
Domenique <domenique_silva@hotmail.com>
2020-11-07 14:17:33 +0100

    add api route that requires authentication

--- js/HTTPauthentication/server.js
+++ js/HTTPauthentication/server.js
@@ -3,15 +3,21 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
+const server = http.createServer(app);
 
 // server config
 app.use(morgan('dev'));
 
-const server = http.createServer(app);
-
 // routes
 app.get('/', (req, res) => {
+  res.statusCode = 200;
   res.send('home.');
+});
+
+// api route requires authentication
+app.get('/api', (req, res) => {
+  res.statusCode = 401;
+  res.end('authentication failed');
 });
 
 server.listen(3000, 'localhost', () => {
