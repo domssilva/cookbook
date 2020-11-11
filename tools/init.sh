f061f9cfe42d6067bf9ec96a798d31a880ccad3f
Domenique Silva <58659500+domssilva@users.noreply.github.com>
2020-11-11 01:55:10 +0100

    Update init.sh

--- tools/init.sh
+++ tools/init.sh
@@ -1,5 +1,6 @@
 # preparing my new vps for HUNTING!!!
 apt-get update
+apt-get install unzip -y
 apt install golang-go -y
 
 # tools
@@ -8,3 +9,8 @@ go get -u github.com/tomnomnom/httprobe
 git clone https://github.com/danielmiessler/SecLists.git
 # https://github.com/michenriksen/aquatone/releases/tag/v1.7.0 get latest release
 wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
+
+# aquatone setup
+unzip aquatone_linux_amd64_1.7.0.zip
+mv aquatone /usr/local/bin/
+rm aquatone_linux_amd64_1.7.0.zip
