7dc24f64b85b888949c36bd6d349c6acb3963cde
Domenique <domenique_silva@hotmail.com>
2020-11-11 01:52:17 +0100

    utils: vps hunter tools

---
+++ tools/init.sh
@@ -0,0 +1,10 @@
+# preparing my new vps for HUNTING!!!
+apt-get update
+apt install golang-go -y
+
+# tools
+go get -u github.com/ffuf/ffuf
+go get -u github.com/tomnomnom/httprobe
+git clone https://github.com/danielmiessler/SecLists.git
+# https://github.com/michenriksen/aquatone/releases/tag/v1.7.0 get latest release
+wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
