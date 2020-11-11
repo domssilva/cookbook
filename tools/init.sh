60c57fcfb965b43738bc83c4e993e09e13ffaf40
Domenique <domenique_silva@hotmail.com>
2020-11-11 02:11:16 +0100

    improve tools script

--- tools/init.sh
+++ tools/init.sh
@@ -6,11 +6,18 @@ apt install golang-go -y
 # tools
 go get -u github.com/ffuf/ffuf
 go get -u github.com/tomnomnom/httprobe
+go get -u -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
 git clone https://github.com/danielmiessler/SecLists.git
 # https://github.com/michenriksen/aquatone/releases/tag/v1.7.0 get latest release
 wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
 
 # aquatone setup
 unzip aquatone_linux_amd64_1.7.0.zip
-mv aquatone /usr/local/bin/
+mv aquatone ./go/bin/
 rm aquatone_linux_amd64_1.7.0.zip
+
+export PATH=$PATH:/root/go/bin/
+
+# perl: http server
+cpan Plack
+plackup -MPlack::App::Directory -e 'Plack::App::Directory->new(root=>".");' -p 8000
