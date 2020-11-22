175532b5b6fcef2ed89ab4f1c05d5b2c1a1b2285
Domenique <domenique_silva@hotmail.com>
2020-11-22 01:01:08 +0100

    [js] - refactor: jwt stuff

--- js/jwt/server.js
+++ js/jwt/server.js
@@ -1,32 +1,33 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const token = jwt.sign({foo: 'bar'}, 'shhh');
+const express = require('express')
+const path = require('path')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
+const token = jwt.sign({foo: 'bar'}, 'shhh')
 
 // config
-app.use(morgan('tiny'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
-});
+  res.sendFile(`${__dirname}/index.html`)
+})
 
 app.get('/login', (req, res) => {
-  res.sendFile(`${__dirname}/login.html`);
-});
+  res.sendFile(`${__dirname}/login.html`)
+})
 
 app.post('/login', (req, res) => {
-  const user = req.body.user;
-  const password = req.body.password;
+  const user = req.body.user
+  const password = req.body.password
 
-  res.send(`hello ${user}`);
-});
+  res.send(`hello ${user}`)
+})
 
 app.get('/test', (req, res) => {
-  res.send('testing...');
-});
+  res.send('testing...')
+})
 
-app.listen(3001, () => console.log('listening on #3001'));
+app.listen(3001, () => console.log('listening on #3001'))
 
