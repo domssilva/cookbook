1392b49b6ef0ad5b30dd38fd7945dafbd2cc6c19
Domenique <domenique_silva@hotmail.com>
2020-11-23 22:40:22 +0100

    [js] - auth mini app

--- js/authorization/server.js
+++ js/authorization/server.js
@@ -1,20 +1,26 @@
-const express = require('express')
-const app = express()
+const app = require('express')()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+
+const PAGES_PATH = `${__dirname}/pages`
 
 // server config
 app.use(morgan('tiny'))
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use('/robots.txt', (req, res) => {
+  res.type('text/plain')
+  res.send("User-agent: *\nDisallow: /admin")
+})
+
 app.get('/', (req, res) => {
   res.redirect('/login')
 })
 
 app.get('/login', (req, res) => {
-  res.sendFile(`${__dirname}/login.html`)
+  res.sendFile(`${PAGES_PATH}/login.html`)
 })
 
 app.post('/login', (req, res) => {
@@ -27,15 +33,25 @@ app.post('/login', (req, res) => {
     res.redirect('/admin')
   }
 
-  res.send(' ... ')
+  res.sendFile(`${PAGES_PATH}/profile.html`)
 })
 
 app.get('/admin', (req, res) => {
-  res.send('welcome admin')
+  
+  const isAdmin = req.cookies["Authorization"] === 'admin:admin'
+
+  if (isAdmin) {
+    res.send('admin panel')
+  }
+
+  res.status(403).send('get out of here!')
 })
 
-app.get('/test', (req, res) => {
-  res.send('testing...')
+app.get('/eatcookies', (req, res) => {
+  console.log('nham nham')
+
+  res.clearCookie('Authorization')
+  res.redirect('/login')
 })
 
 app.listen(3001, () => console.log('listening on #3001'))
