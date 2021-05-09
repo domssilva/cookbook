a07d67e071a2aa2f89849f4d2690a3e46133752d
Domenique <domenique_silva@hotmail.com>
2021-05-09 18:27:46 +0200

    [js] - leetcode challenge, array manipulation with pointers

---
+++ js/problem-solving/main.js
@@ -0,0 +1,18 @@
+console.clear()
+
+const array = [1, 3, 7, 9, 2]
+const target = 11
+
+/*
+  Given an array of integers, return the indices of the two numbers
+  that add up to a given target.
+
+  1. verify constrains
+    thinking about all the edge cases
+*/
+
+for (let i = 0; i < array.length; i++) {
+  let currentNum = array[i]
+  let nextNum = array[i+1]
+  console.log(array[i])
+}
