43a5d6470f5fffa68fe936542e815a5e5b1306ad
domssilva <domenique_silva@hotmail.com>
2023-01-07 13:28:29 +0100

    feature: add new customer endpoint

--- python/customers-xml-api/main.py
+++ python/customers-xml-api/main.py
@@ -10,6 +10,25 @@ customers = []
 def home():
     return "pong."
 
+# POST /api/customer
+@app.route('/api/customer', methods=['POST'])
+def create_customer():
+    # Parse the request body as XML
+    customer_xml = ET.fromstring(request.data)
+    
+    # Deserialize the customer from the XML element
+    customer = customer_from_xml(customer_xml)
+    
+    # Generate a new customer ID for the customer
+    customer['id'] = generate_customer_id()
+    
+    # Add the customer to the list
+    customers.append(customer)
+    
+    # Return the new customer ID as the response
+    response = make_response(str(customer['id']))
+    response.headers['Content-Type'] = 'text/plain'
+    return response
 
 if __name__ == '__main__':
     app.run(host='localhost', port=3000)
