70462d22dfd5f1ef59cc80e2eae62b7445a3ff10
domssilva <domenique_silva@hotmail.com>
2023-05-06 17:31:01 -0400

    basic-auth(go): handle /signup route + create account

--- go/basic-auth/go.mod
+++ go/basic-auth/go.mod
@@ -2,4 +2,7 @@ module basic-auth
 
 go 1.19
 
-require github.com/go-sql-driver/mysql v1.7.1 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+)
--- go/basic-auth/main.go
+++ go/basic-auth/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
+	http.HandleFunc("/signup", logRequest(handleSignup))
 	http.HandleFunc("/", logRequest(handleHome))
 	http.ListenAndServe(":8080", nil)
 }
@@ -14,6 +16,53 @@ func main() {
 func handleHome(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		http.ServeFile(w, r, "index.html")
+	} else {
+		http.Error(w, "Method not allowed.", http.StatusMethodNotAllowed)
+	}
+}
+
+// Handles / signup
+func handleSignup(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		http.ServeFile(w, r, "sign.html")
+	} else if r.Method == http.MethodPost {
+		// Parse the username and password from the request body
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Invalid form data", http.StatusBadRequest)
+			return
+		}
+
+		username := r.PostForm.Get("username")
+		password := r.PostForm.Get("password")
+
+		// fetch db
+		db, err := GetDbConnection()
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Error fetching db on signup.", http.StatusInternalServerError)
+		}
+		// ensure that the db connection is properly closed
+		defer db.Close()
+
+		// Hash the password
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+			http.Error(w, "Could not hash password", http.StatusInternalServerError)
+			return
+		}
+
+		// insert credentials using Parameterized Strings to avoid SQLi
+		_, err = db.Exec("INSERT INTO users (username, password) VALUES (?, ?)", username, hashedPassword)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Could not create user", http.StatusInternalServerError)
+			return
+		}
+
+		// Return a success response
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("New account created."))
 	} else {
 		http.Error(w, "Method not allowed.", http.StatusMethodNotAllowed)
 	}
